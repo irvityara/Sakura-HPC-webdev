69da88e3d7882a0d80e4cdafb0212d680845acdc
Irvi <irvityara456@gmail.com>
2023-06-04 11:56:25 +0700

    initial commit

---
+++ dist/script/index.js
@@ -0,0 +1,8 @@
+//index.js
+
+import app from "./views/app.js";
+
+import "./components/get-know-japan-element.js";
+import "./components/photo-from-tourist-element.js";
+
+document.addEventListener("DOMContentLoaded", app);
